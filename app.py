2ca115eb40a97ef1c4feab73c9ce5dceefe051fa
Dylan Barva <d.barva5402@gmail.com>
2021-11-15 22:20:38 -0800

    finish app views + data model

--- app.py
+++ app.py
@@ -16,4 +16,16 @@ def test_submit():
 @app.route("/team/<int:team>")
 def team_view(team : int):
     title_string = "Team View: " + str(team)
-    return flask.render_template("team_view.html", title=title_string, headers=data.raw_match_data_headers, raw_match_data = data.raw_match_data, highlights=data.analyzed_data_highlights)
+    return flask.render_template("team_view.html", title=title_string, headers=data.raw_match_data_headers_687, raw_match_data = data.raw_match_data_687, highlights=data.analyzed_data_team_highlights)
+
+@app.route("/data/analyzed")
+def analyzed_data():
+    return flask.render_template("tabular_data_view.html", title="Analyzed Data", headers=data.analyzed_data_headers, rows=data.analyzed_data)
+
+@app.route("/data/ranked")
+def ranked_data():
+    return flask.render_template("tabular_data_view.html", title="Ranked Data", headers=data.ranked_data_headers, rows=data.ranked_data)
+
+@app.route("/data/raw")
+def raw_data():
+    return flask.render_template("tabular_data_view.html", title="Raw Data", headers=data.raw_match_data_headers, rows=data.raw_match_data)
