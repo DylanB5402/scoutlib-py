7bf096b548a06bc5d85d9432fdecda8fcbc03e25
Dylan Barva <d.barva5402@gmail.com>
2021-12-25 16:01:00 -0800

    code for switching between graphs works

--- static/graph_selection.js
+++ static/graph_selection.js
@@ -1,17 +1,38 @@
-var current_chart = "teleop"
-        $('#auto').click(()=> {
-            if (current_chart != "auto") {
-                current_chart = "auto"
-                console.log(687)
-            }
-        })
+var current_chart = ""
 
-// var data = [
-//         {
-//             x: ['giraffes', 'orangutans', 'monkeys'],
-//             y: [20, 14, 23],
-//             type: 'bar'
-//         }
-//         ];
+$('#teleop').click(()=> {
+    if (current_chart != "teleop") {
+        current_chart = "teleop"
+        make_graph(['a', 'b', 'c'], [30, 20, 10])
+        // console.log(987)
+    }
+})
 
-// Plotly.newPlot('data_graph', data);
+$('#auto').click(()=> {
+    if (current_chart != "auto") {
+        current_chart = "auto"
+        make_graph(['a', 'b', 'c'], [20, 10, 30])
+        // console.log(687)
+    }
+})
+
+$('#climb').click(()=> {
+    if (current_chart != "climb") {
+        current_chart = "climb"
+        // console.log(1323)
+        make_graph(['a', 'b', 'c'], [10, 20, 30])
+    }
+})
+
+
+
+function make_graph(x_labels, y_data) {
+    var data = [
+        {
+            x: x_labels,
+            y: y_data,
+            type: 'bar'
+        }
+    ];
+    Plotly.newPlot('data_graph', data);
+}
