75c9c4589fc433af7f38dbd237e9b03719f3de33
Dylan Barva <d.barva5402@gmail.com>
2021-12-25 16:06:43 -0800

    clean up plot gen code

--- static/graph_selection.js
+++ static/graph_selection.js
@@ -1,10 +1,11 @@
+// graph data specification: {'x_labels' : [label1, label2, ...], 'y_data' : [10, 20, 30, ...]}
+
 var current_chart = ""
 
 $('#teleop').click(()=> {
     if (current_chart != "teleop") {
         current_chart = "teleop"
         make_graph(['a', 'b', 'c'], [30, 20, 10])
-        // console.log(987)
     }
 })
 
@@ -12,19 +13,15 @@ $('#auto').click(()=> {
     if (current_chart != "auto") {
         current_chart = "auto"
         make_graph(['a', 'b', 'c'], [20, 10, 30])
-        // console.log(687)
     }
 })
 
 $('#climb').click(()=> {
     if (current_chart != "climb") {
         current_chart = "climb"
-        // console.log(1323)
         make_graph(['a', 'b', 'c'], [10, 20, 30])
     }
 })
-
-
 
 function make_graph(x_labels, y_data) {
     var data = [
